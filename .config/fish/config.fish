3228733cb886068d4ae27883f764691ded019e55
= <johnalipscomb@gmail.com>
2022-08-29 17:53:21 -0500

    Update fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr -a pvim pdm run lvim
-
 nvm use 16.16.0
+
+bass export GTK_IM_MODULE=ibus
+bass export XMODIFIERS=@im=ibus
+bass export QT_IM_MODULE=ibus
 
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -14,6 +14,7 @@ lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
 vim.opt.relativenumber = true
 vim.opt.list = true
+vim.opt.clipboard = "unnamedplus"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -171,7 +172,7 @@ lvim.plugins = {
     event = "BufRead",
     config = function()
       vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
+      vim.g.gitblame_enabled = 1
     end,
   },
   {
