1ffd392913419e04d8263e1c14aa71fbaa9096fc
= <johnalipscomb@gmail.com>
2022-09-03 16:05:23 -0500

    Update lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -68,230 +68,182 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"css",
+	"fish",
+	"java",
+	"javascript",
+	"json",
+	"lua",
+	"markdown",
+	"python",
+	"rust",
+	"tsx",
+	"typescript",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
+require("lspconfig").racket_langserver.setup({})
 
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
--- vim.tbl_map(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
-require 'lspconfig'.racket_langserver.setup {}
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  { command = "rustfmt", filetypes = { "rust" } },
-  { command = "shfmt", filetypes = { "bash" } },
-  { command = "stylua", filetypes = { "lua" } },
-  {
-    command = "prettier",
-    extra_args = { "--print-with=100" },
-    filetypes = { "typescript", "typescriptreact", "html", "css", "md", "markdown", "yaml", "yml" },
-  },
-  --   {
-  --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --     command = "prettier",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     extra_args = { "--print-with", "100" },
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "typescript", "typescriptreact" },
-  --   },
-}
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "black", filetypes = { "python" } },
+	{ command = "isort", filetypes = { "python" } },
+	{ command = "rustfmt", filetypes = { "rust" } },
+	{ command = "shfmt", filetypes = { "bash" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{
+		command = "prettier",
+		extra_args = { "--print-width", "80" },
+		filetypes = { "typescript", "typescriptreact", "html", "css", "markdown", "yaml" },
+	},
+	{ command = "sql-formatter", filetypes = { "sql" } },
+})
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "shellcheck", filetypes = { "sh", "bash" } },
+	{ command = "checkmake", filetypes = { "Makefile" } },
+	{ command = "hadolint", filetypes = { "docker" } },
+})
 
 -- Additional Plugins
 lvim.plugins = {
-  -- { "folke/tokyonight.nvim" },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "sindrets/diffview.nvim",
-    event = "BufRead",
-  },
-  {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 1
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
-    end
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
-    config = function()
-      vim.g.mkdp_auto_start = 1
-    end,
-  },
-  {
-    "itchyny/vim-cursorword",
-    event = { "BufEnter", "BufNewFile" },
-    config = function()
-      vim.api.nvim_command("augroup user_plugin_cursorword")
-      vim.api.nvim_command("autocmd!")
-      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-      vim.api.nvim_command("augroup END")
-    end
-  },
-  {
-    "folke/todo-comments.nvim",
-    event = "BufRead",
-    config = function()
-      require("todo-comments").setup()
-    end,
-  },
-  {
-    "rmagatti/goto-preview",
-    config = function()
-      require('goto-preview').setup {
-        width = 120; -- Width of the floating window
-        height = 25; -- Height of the floating window
-        default_mappings = false; -- Bind default mappings
-        debug = false; -- Print debug information
-        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
-        -- You can use "default_mappings = true" setup option
-        -- Or explicitly set keybindings
-        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
-      }
-    end
-  },
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require('mini.indentscope').setup {
-        draw = {
-          -- Delay (in ms) between event and start of drawing scope indicator
-          delay = 100,
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufRead",
+		setup = function()
+			vim.g.indentLine_enabled = 1
+			vim.g.indent_blankline_char = "▏"
+			vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+			vim.g.indent_blankline_buftype_exclude = { "terminal" }
+			vim.g.indent_blankline_show_trailing_blankline_indent = false
+			vim.g.indent_blankline_show_first_indent_level = false
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		ft = "markdown",
+		config = function()
+			vim.g.mkdp_auto_start = 1
+		end,
+	},
+	{
+		"itchyny/vim-cursorword",
+		event = { "BufEnter", "BufNewFile" },
+		config = function()
+			vim.api.nvim_command("augroup user_plugin_cursorword")
+			vim.api.nvim_command("autocmd!")
+			vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+			vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+			vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+			vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+			vim.api.nvim_command("augroup END")
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({
+				width = 120, -- Width of the floating window
+				height = 25, -- Height of the floating window
+				default_mappings = false, -- Bind default mappings
+				debug = false, -- Print debug information
+				opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+				post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+				-- You can use "default_mappings = true" setup option
+				-- Or explicitly set keybindings
+				-- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+				-- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+				-- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.indentscope").setup({
+				draw = {
+					-- Delay (in ms) between event and start of drawing scope indicator
+					delay = 100,
 
-          -- Animation rule for scope's first drawing. A function which, given next
-          -- and total step numbers, returns wait time (in ms). See
-          -- |MiniIndentscope.gen_animation()| for builtin options. To not use
-          -- animation, supply `require('mini.indentscope').gen_animation('none')`.
-          -- animation = --<function: implements constant 20ms between steps>,
-        },
+					-- Animation rule for scope's first drawing. A function which, given next
+					-- and total step numbers, returns wait time (in ms). See
+					-- |MiniIndentscope.gen_animation()| for builtin options. To not use
+					-- animation, supply `require('mini.indentscope').gen_animation('none')`.
+					-- animation = --<function: implements constant 20ms between steps>,
+				},
 
-        -- Module mappings. Use `''` (empty string) to disable one.
-        mappings = {
-          -- Textobjects
-          object_scope = 'ii',
-          object_scope_with_border = 'ai',
+				-- Module mappings. Use `''` (empty string) to disable one.
+				mappings = {
+					-- Textobjects
+					object_scope = "ii",
+					object_scope_with_border = "ai",
 
-          -- Motions (jump to respective border line; if not present - body line)
-          goto_top = '[i',
-          goto_bottom = ']i',
-        },
+					-- Motions (jump to respective border line; if not present - body line)
+					goto_top = "[i",
+					goto_bottom = "]i",
+				},
 
-        -- Options which control computation of scope. Buffer local values can be
-        -- supplied in buffer variable `vim.b.miniindentscope_options`.
-        options = {
-          -- Type of scope's border: which line(s) with smaller indent to
-          -- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
-          border = 'both',
+				-- Options which control computation of scope. Buffer local values can be
+				-- supplied in buffer variable `vim.b.miniindentscope_options`.
+				options = {
+					-- Type of scope's border: which line(s) with smaller indent to
+					-- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+					border = "both",
 
-          -- Whether to use cursor column when computing reference indent. Useful to
-          -- see incremental scopes with horizontal cursor movements.
-          indent_at_cursor = true,
+					-- Whether to use cursor column when computing reference indent. Useful to
+					-- see incremental scopes with horizontal cursor movements.
+					indent_at_cursor = true,
 
-          -- Whether to first check input line to be a border of adjacent scope.
-          -- Use it if you want to place cursor on function header to get scope of
-          -- its body.
-          try_as_border = false,
-        },
+					-- Whether to first check input line to be a border of adjacent scope.
+					-- Use it if you want to place cursor on function header to get scope of
+					-- its body.
+					try_as_border = false,
+				},
 
-        -- Which character to use for drawing scope indicator
-        symbol = '╎',
-      }
-    end
-  },
-  {
-    "tpope/vim-surround",
-    keys = { "c", "d", "y" }
-    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-    -- setup = function()
-    --  vim.o.timeoutlen = 500
-    -- end
-  },
-  { "mg979/vim-visual-multi", }
+				-- Which character to use for drawing scope indicator
+				symbol = "╎",
+			})
+		end,
+	},
+	{
+		"tpope/vim-surround",
+		keys = { "c", "d", "y" },
+		-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+		-- setup = function()
+		--  vim.o.timeoutlen = 500
+		-- end
+	},
+	{ "mg979/vim-visual-multi" },
+	{
+		"f-person/git-blame.nvim",
+		event = "BufRead",
+		config = function()
+			vim.cmd("highlight default link gitblame SpecialComment")
+			vim.g.gitblame_enabled = 0
+		end,
+	},
 }
-
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
